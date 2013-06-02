0ff3cd42bb657e9332c00095b4cb3863d8d8ece9
bob <dariboufix@daribouca.net>
2013-06-02 13:13:57 +0200

    initial import

---
+++ SAVED_GAMES/Liveries/uh-1h/Bob/description.lua
@@ -0,0 +1,24 @@
+livery = {
+	{"uh1_main_dam", 0, "tdc_camo_bob", false};
+	{"uh1_main_dam", 2, "uh1_spec.bmp", true};
+	{"uh1_tail_dam", 0, "tdc_camo_bob_tail", false};
+	{"uh1_tail_dam", 2, "uh1_tail_spec.bmp", true};
+	{"uh1_main", 0, "tdc_camo_bob", false};
+	{"uh1_main", 2, "uh1_spec.bmp", true};
+	{"uh1_tail", 0, "tdc_camo_bob_tail", false};
+	{"uh1_tail", 2, "uh1_tail_spec.bmp", true};	
+	{"uh1_weapon", 0, "uh1_weapon.bmp", true};
+	{"pilot_UH1", 0, "pilot_UH1.bmp", true};
+	{"pilot_UH1_helmet", 0, "pilot_UH1_helmet_B.bmp", true};
+	{"pilot_UH1_patch", 0, "pilot_UH1_patch_1_cavalery.tga", true};
+	{"Gunner_UH1", 0, "gunner_UH1.bmp", true};
+	{"uh1_cov", 0, "uh1_cover.bmp", true};
+	{"UH1-cpt-door", 0 ,"UH1_door_c",true};
+	{"UH1-cpt_karkas", 0 ,"UH1_karkas_c",true};
+	{"UH1-cpt_karkas2", 0 ,"UH1_karkas2_c",true};
+	{"UH1-cpt_panel", 0 ,"UH1_panel_c",true};
+	{"UH1-cpt_central_box", 0 ,"UH1_boxs_c",true};
+	{"UH1-cpt_dev2", 0 ,"UH1_dev_2",true};
+}
+
+countries = {"USA"}
